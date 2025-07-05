81114623409e746d83d2dd77e58f4eca7deda734
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-07-05 12:46:13 +0200

    Add logic to DOM loade, it cannot load empty arr

--- src/js/dom-manipulator.js
+++ src/js/dom-manipulator.js
@@ -1,31 +1,54 @@
 import { projectOpener, Projects } from "./todo-projects";
 export class DOM{
+  content = document.querySelector('#content')
   static firstLoadInit(){
     for(let i = 0; i < Projects.projects.length; i++){
     let div = document.createElement('div')
+    div.style.width = '200px'
     div.textContent = Projects.projects[i].title
     div.id = Projects.projects[i].title
-    document.body.appendChild(div)
+    content.appendChild(div)
     }
   }
   static openToDo(){
     document.addEventListener('click', event => {
       console.log(event.target.id)
-      while(document.body.lastChild){
-        document.body.removeChild(document.body.lastChild)
+      const target = event.target.id
+      if(this.targetCheck(target)){
+      while(content.lastChild){
+        content.removeChild(content.lastChild)
       }
-      let target = projectOpener(event.target.id)
-      target.toDos.map(item => {
+      let currentProject = projectOpener(event.target.id)
+      currentProject.toDos.map(item => {
+      const toDoDiv = document.createElement('div')
       const title = document.createElement('h2')
       const description = document.createElement('p')
       const priority = document.createElement('p')
+      const dueDate = document.createElement('p')
+      const checkList = document.createElement('p')
+      toDoDiv.id = item.title
       title.textContent = item.title
       description.textContent = item.description
       priority.textContent = item.priority
-      document.body.appendChild(title)
-      document.body.appendChild(description)
-      document.body.appendChild(priority)
+      dueDate.textContent = item.dueDate
+      checkList.textContent = item.checkList
+      toDoDiv.appendChild(title)
+      toDoDiv.appendChild(description)
+      toDoDiv.appendChild(priority)
+      toDoDiv.appendChild(dueDate)
+      toDoDiv.appendChild(checkList)
+      content.appendChild(toDoDiv)
       })
+      }
     })
+  }
+  static targetCheck(target){
+    let conditionCheck = Projects.projects.reduce((accumulator,currentVal) => {
+      if(target === currentVal.title){
+        accumulator = true
+      }
+      return accumulator
+    },false)
+    return conditionCheck
   }
 }
